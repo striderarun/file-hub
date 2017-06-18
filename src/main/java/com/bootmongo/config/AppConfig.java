a1e05e0fdd53c184f0b74eed6c5c52e713ddcde0
strider90arun <strider90arun@gmail.com>
2017-06-17 21:38:43 -0700

    Upgrade to Spring Boot

--- src/main/java/com/bootmongo/config/AppConfig.java
+++ src/main/java/com/bootmongo/config/AppConfig.java
@@ -1,8 +1,16 @@
 package com.bootmongo.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@Configuration
+@ComponentScan(basePackages = "com.bootmongo")
+@EnableMongoRepositories
+@EnableAutoConfiguration
 @SpringBootApplication
 public class AppConfig {
 
